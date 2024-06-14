ccd0409fe99d5af8bc764bb0ecf38ecacb6850a1
funa <orz.home35@gmail.com>
2024-06-14 16:50:22 +0900

    240614_GD子ページ追加

--- test/main.js
+++ test/main.js
@@ -15,39 +15,31 @@ $('a[href*="index.html#"]').click(function () {
 	return false;
 });
 
-// ヘッダー登場
-$(window).scroll(function () {           /* スクロールされた時 */
-  var pos = $('.about').offset();          /* mvを過ぎたmainタグの高さを取得して変数[pos]に格納 */
-  if ($(this).scrollTop() > pos.top) {   /* 変数[pos]より、スクロールされていたら */
-    $('header').fadeIn();                /* ヘッダーをふわっと表示 */
-  } else {                               /* それ以外の場合 */
-    $('header').fadeOut();               /* ヘッダーをふわっと非表示 */
-  }
-});
+
+// ウィンドウがスクロールされるたびにこの関数が呼び出される
+window.onscroll = function() {
+    var video = document.getElementById('video-fix');
+
+    // ビデオの現在の再生位置を取得
+    var currentTime = video.currentTime;
+
+    // ページのスクロール量を取得
+    var scrollPosition = window.scrollY;
+
+    // ページの高さとビデオの長さを取得
+    var documentHeight = document.documentElement.scrollHeight - window.innerHeight;
+    var videoDuration = video.duration;
+
+    // スクロール位置をビデオの再生位置にマッピング
+    if (videoDuration) { // 動画のメタデータが読み込まれているか確認
+        var newTime = (scrollPosition / documentHeight) * videoDuration;
+        video.currentTime = newTime;
+    }
+};
+
+// ビデオがロードされたときに自動再生を開始
+// document.getElementById('fixed-video').addEventListener('loadeddata', function() {
+//     this.play();
+// });
 
 
-$('.slider-inner').slick({
-	autoplay: true,//自動的に動き出すか。初期値はfalse。
-	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-	slidesToShow: 3,//スライドを画面に3枚見せる
-	slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
-	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-	dots: true,//下部ドットナビゲーションの表示
-	responsive: [
-		{
-		breakpoint: 900,//モニターの横幅が769px以下の見せ方
-		settings: {
-			slidesToShow: 2,//スライドを画面に2枚見せる
-			slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
-		}
-	},
-	{
-		breakpoint: 650,//モニターの横幅が426px以下の見せ方
-		settings: {
-			slidesToShow: 1,//スライドを画面に1枚見せる
-			slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-		}
-	}
-]
-});
