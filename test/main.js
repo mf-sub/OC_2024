041d6679c1aadde5f521760c4401ee37b58cdc6e
funa <orz.home35@gmail.com>
2024-06-20 22:34:46 +0900

    240620_モーション試行錯誤

--- test/main.js
+++ test/main.js
@@ -34,25 +34,25 @@ $('a[href*="index.html#"]').click(function () {
 
 
 // ウィンドウがスクロールされるたびにこの関数が呼び出される
-window.onscroll = function() {
-    var video = document.getElementById('video-fix');
+// window.onscroll = function() {
+//     var video = document.getElementById('video-fix');
 
-    // ビデオの現在の再生位置を取得
-    var currentTime = video.currentTime;
+//     // ビデオの現在の再生位置を取得
+//     var currentTime = video.currentTime;
 
-    // ページのスクロール量を取得
-    var scrollPosition = window.scrollY;
+//     // ページのスクロール量を取得
+//     var scrollPosition = window.scrollY;
 
-    // ページの高さとビデオの長さを取得
-    var documentHeight = document.documentElement.scrollHeight - window.innerHeight;
-    var videoDuration = video.duration;
+//     // ページの高さとビデオの長さを取得
+//     var documentHeight = document.documentElement.scrollHeight - window.innerHeight;
+//     var videoDuration = video.duration;
 
-    // スクロール位置をビデオの再生位置にマッピング
-    if (videoDuration) { // 動画のメタデータが読み込まれているか確認
-        var newTime = (scrollPosition / documentHeight) * videoDuration;
-        video.currentTime = newTime;
-    }
-};
+//     // スクロール位置をビデオの再生位置にマッピング
+//     if (videoDuration) { // 動画のメタデータが読み込まれているか確認
+//         var newTime = (scrollPosition / documentHeight) * videoDuration;
+//         video.currentTime = newTime;
+//     }
+// };
 
 
 // ビデオがロードされたときに自動再生を開始
