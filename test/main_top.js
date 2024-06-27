0980349b12bd3cb963c707f6303615a83f02ce6b
funa <orz.home35@gmail.com>
2024-06-27 20:11:58 +0900

    240627_icon

--- test/main_top.js
+++ test/main_top.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // img要素のsrc属性を変更
+    var imgElement = document.querySelector('img#shutter-video');
+    if (imgElement) {
+        imgElement.src = 'images/mv_opening.png';
+    }
+
+    // div要素にクラスを追加
+    var divElement = document.querySelector('div.shutter');
+    if (divElement) {
+        divElement.classList.add('shutter-up');
+    }
+});
+
+
 
 var SIZE = 290;
 var tmp = {};
@@ -10,30 +25,24 @@ function loadImageToTmp(start, end) {
     let imagesLoaded = 0;
 	if (start > end) {
     for (var i = start; i >= end; i--) {
-		console.log("loadImageToTmp called with start:", start, "end:", end);
-        // console.log("loadImageToTmp called with start:", start, "end:", end);
+
         const _i = i;
         const img = new Image();
         tmp[_i] = null;
         const filename = "images/mv_sp//mv_sp_" + String(_i).padStart(5, '0') + ".png";
         img.src = filename;
-        // console.log("Loading image:", filename);
 
         img.addEventListener("load", () => {
             tmp[_i] = img;
             imagesLoaded++;
-            // console.log(`Image ${_i} loaded`);
 
-            // Check if all images are loaded
             if (imagesLoaded === (start - end + 1)) {
-                // console.log("All images loaded");
                 startImageSwitch(start, end);
             }
         });
     }
 } else {
 	 for (var i = start; i <= end; i++) {
-		console.log("loadImageToTmp called with start:", start, "end:", end);
         const _i = i;
         const img = new Image();
         tmp[_i] = null;
@@ -44,7 +53,6 @@ function loadImageToTmp(start, end) {
             tmp[_i] = img;
             imagesLoaded++;
 
-            // Check if all images are loaded
             if (imagesLoaded === (end - start + 1)) {
                 startImageSwitch(start, end);
             }
@@ -55,9 +63,8 @@ function loadImageToTmp(start, end) {
 
 function startImageSwitch(start, end) {
     let currentIndex = start;
-	let interval = 18; // 初期値は20 milliseconds
+	let interval = 18;
 
-    // box4 から box5 に移動する場合、もしくは box5 から box4 に移動する場合は interval を 10 に設定する
     if (start === 95 && end === 157){
         interval = 6;
     }
@@ -72,11 +79,10 @@ function startImageSwitch(start, end) {
     }
 
     function switchImage() {
-        if (start > end) { // Forward direction
+        if (start > end) {
             if (currentIndex >= end) {
                 if (tmp[currentIndex]) {
                     imgElement.src = tmp[currentIndex].src;
-                    // console.log(`Image switched to ${tmp[currentIndex].src}`);
                 }
                 currentIndex--;
                 setTimeout(switchImage, interval);
@@ -106,7 +112,6 @@ $(document).ready(function() {
 	$.scrollify.move("#box1");
 
 	$('#menu').on('click', function() {
-		console.log("box1 clicked");
 	  // panelsを取得
 	  window.scrollTo(0, document.body.scrollHeight);
 	  var panels = $.map($(".box").add("#footer"), function(element) {
@@ -131,7 +136,7 @@ $(document).ready(function() {
             ref =$(panels[i]).attr("id");// 現在のセクションの id を取得する
             currentIndex = sections.indexOf(ref);
             previousIndex = sections.indexOf(lastIndex);
-			console.log("panels:", panels, "i", i, "Current section id:", ref, "Current index:", currentIndex, "Previous index:", previousIndex);
+
             if(-1 === previousIndex || -1 === currentIndex) {
 				if (ref === "box1") {
 					loadImageToTmp(290, 289);
@@ -168,27 +173,21 @@ $(document).ready(function() {
                 }
             }
 			lastIndex = ref;
-			console.log("Last index set to", lastIndex);
 			setBkHeightForBoxes();
         }
     });
 
-// box1とbox2の高さをbkに設定する関数
 function setBkHeightForBoxes() {
-    // box1の高さを取得
     var box1Height = document.querySelector('#box1').scrollHeight;
     var box2Height = document.querySelector('#box2').scrollHeight;
 	var box3Height = document.querySelector('#box3').scrollHeight;
 	var box4Height = document.querySelector('#box4').scrollHeight;
 	var box5Height = document.querySelector('#box5').scrollHeight;
 
-	// .bk 要素の高さを .contents 要素の高さと同じに設定
     document.querySelectorAll('.bk').forEach(function(bk) {
         bk.style.height = box1Height + box2Height + box3Height + box4Height + box5Height + 'px';
-		// console.log("box1Height", box1Height, "box2Height", box2Height);
-		console.log("bk height set to", box1Height + box2Height + box3Height + box4Height + box5Height);
     });
 }
 window.addEventListener('load', setBkHeightForBoxes);
-// ウィンドウのサイズが変更されたときに高さを設定する関数を呼び出す
+
 window.addEventListener('resize', setBkHeightForBoxes);
