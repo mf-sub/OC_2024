07903e3a5f0301af39c7e6b6ce81451e5ee5b134
funa <orz.home35@gmail.com>
2024-07-01 11:58:58 +0900

    240630_一部修正

--- test/main.js
+++ test/main.js
@@ -102,3 +102,16 @@ $('.fadeUpTrigger').each(function(){ //fadeInUpTriggerというクラス名が
 	$(window).on('load', function(){
 	fadeAnime();/* アニメーション用の関数を呼ぶ*/
 	});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+
+//アコーディオンをクリックした時の動作
+$('.accordion-title').on('click', function() {//タイトル要素をクリックしたら
+	var findElm = $(this).next(".accordion-cont");//直後のアコーディオンを行うエリアを取得し
+	$(findElm).slideToggle();//アコーディオンの上下動作
+
+	if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+		$(this).removeClass('close');//クラス名を除去し
+	}else{//それ以外は
+		$(this).addClass('close');//クラス名closeを付与
+	}
+});
